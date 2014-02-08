bf8aafd6e8fec4c5ae64599794ebb327960154a3
Rami Al-Ghanmi <rami@ramialghanmi.com>
2014-02-08 10:08:08 -0800

    updated sql schema to work with the extra needs

--- db/schema.sql
+++ db/schema.sql
@@ -21,8 +21,6 @@ CREATE TABLE IF NOT EXISTS course (
 	PRIMARY KEY(course_id)
 );
 
-INSERT INTO course VALUES('CSCI 104', 'Data Structures and Object Oriented Design', 'Introduces the student to standard data structures (linear structures such as linked lists, (balanced) trees, priority queues, and hashtables), using the C++ programming language.Prerequisite: CSCI 103 and CSCI 109. Corequisite: CSCI 170.', 4);
-
 CREATE TABLE IF NOT EXISTS section (
 	course_id		CHAR,
 	section_id		CHAR,
@@ -40,18 +38,6 @@ CREATE TABLE IF NOT EXISTS section (
 	PRIMARY KEY(section_id, semester),
 	FOREIGN KEY(course_id) REFERENCES course(course_id)
 );
-
-INSERT INTO section VALUES('CSCI 104', '29907', '20141', 'Tue,Thu', '09:30am', '10:50am', 'SOS', 'B46', 1, 0);
-INSERT INTO section VALUES('CSCI 104', '29905', '20141', 'Tue,Thu', '11:00am', '12:20pm', 'ZHS', '352', 1, 0);
-INSERT INTO section VALUES('CSCI 104', '30397', '20141', 'Tue,Thu', '12:30pm', '01:50pm', 'ZHS', '352', 1, 0);
-INSERT INTO section VALUES('CSCI 104', '30294', '20141', 'Tue', '03:00pm', '04:50pm', 'WPH', 'B36', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '30200', '20141', 'Tue', '03:30pm', '05:20pm', 'SAL', '126', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '30238', '20141', 'Tue', '05:30pm', '07:20pm', 'SAL', '126', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '29915', '20141', 'Wed', '12:00pm', '01:50pm', 'SAL', '126', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '30394', '20141', 'Wed', '02:00pm', '03:50pm', 'SAL', '109', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '29914', '20141', 'Wed', '03:00pm', '04:50pm', 'SAL', '126', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '29912', '20141', 'Wed', '05:00pm', '06:50pm', 'SAL', '109', 0, 1);
-INSERT INTO section VALUES('CSCI 104', '30379', '20141', 'Thu', '05:00pm', '06:50pm', 'SAL', '127', 0, 1);
 
 CREATE TABLE IF NOT EXISTS student (
 	usc_id			CHAR,
@@ -91,9 +77,6 @@ CREATE TABLE IF NOT EXISTS homework (
 	PRIMARY KEY(course_id, semester, hw_id),
 	FOREIGN KEY(course_id) REFERENCES course(course_id)
 );
-
-INSERT INTO homework VALUES('CSCI 104', '20141', 'HW01', 'HW01', 'Course Overview and Review', '2014-01-22T11:59:59-08:00');
-INSERT INTO homework VALUES('CSCI 104', '20141', 'HW02', 'HW02', 'Recursion and Linked Lists', '2014-01-31T11:59:59-08:00');
 
 CREATE TABLE IF NOT EXISTS github_account (
 	student_id		CHAR,
